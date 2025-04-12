45fe46a7d7418556c22fe47868223d3dce5538e2
zhl27 <lucazs124@gmail.com>
2025-04-12 10:45:01 -0300

    .

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import express from "express"
+import "dotenv/config"
+
+
+
 
 const app = express()
 
@@ -8,7 +12,23 @@ app.get("/healthcheck", (req, res) => {
 	})
 })
 
+app.get("/api/v1/productos", (req, res) => {
+	const price_lt = req.query.price_lt // siempre recibimos strings a través de los querys
+
+	if(!query_lt) {
+		res.json(productos.map(p => {
+			return {
+				"precioBase": p.precioBase,
+				"nombre": p.nombre
+			}	
+		})
+	}
+	
+	// res.json(productos)
+})
+
 // uses env SERVER_PORT, but if non existent, then it fallbacks to default value 3000
-app.listen(process.env.SERVER_PORT ?? 3000, () => {
+const port = process.env.SERVER_PORT ?? 3000
+app.listen(port, () => {
 	console.log("Server listening to port 3000")
 })
